be27c4ad21964ab263abf9ad38d5dc17a11b36ec
Talha Khan <mail.talha.pro@gmail.com>
2020-02-27 19:11:54 +0500

    Boiler Plate Setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Fintrack`,
+    description: `Fintrack by Hileets.`,
+    author: `@hileets`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,10 +15,11 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Fintrack`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,

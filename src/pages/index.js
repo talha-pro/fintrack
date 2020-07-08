98bc5babf0fc8d75dba0ba6201e45ce1aee032e4
Talha <mail.talha.pro@gmail.com>
2020-07-08 21:56:59 +0500

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ import mockup from "../assets/mockup.png"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Hileets" />
+    <SEO title="Hileet" />
     <NavBar />
     <section className="header d-flex align-items-center">
       <Container className="header__container">
